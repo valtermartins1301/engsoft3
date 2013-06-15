8451aede0798ccbfa4ea391de48ab778ec8b54d0
lindomar.reitz@gmail.com <lindomar.reitz@gmail.com@30d60ad6-610f-614d-8bfc-85eb27d9e81c>
2013-06-15 22:00:06 +0000

    Criação dos métodos do dao de contas e padronização do persistence.xml

--- sigecon/src/br/sigecon/daos/ContasDAO.java
+++ sigecon/src/br/sigecon/daos/ContasDAO.java
@@ -1,5 +1,132 @@
 package br.sigecon.daos;
 
-public class ContasDAO {
+import java.util.ArrayList;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import br.sigecon.beans.Conta;
+import br.sigecon.beans.ContaCorrente;
+import br.sigecon.beans.Lancamento;
+
+public class ContasDAO {
+	private EntityManager entityManager;
+	private EntityManagerFactory factory;
+	
+	public ContasDAO() {
+		factory = Persistence.createEntityManagerFactory("persist");
+		entityManager = factory.createEntityManager();
+	}
+	
+	public ArrayList<Lancamento> consultarLancamento(Conta conta) {
+		return null;	
+	}
+	
+	public Conta[] consultarSaldo(Conta conta) {
+		return null;
+	}
+	
+	public ArrayList<Lancamento> faturaCartaoCredito(Conta conta) {
+		return null;
+	}
+	
+	public void merge(Conta conta) {
+		try {
+			EntityTransaction transaction = entityManager.getTransaction();
+			try {
+				transaction.begin();
+				
+				ContaCorrente contaCorrente = entityManager.find(ContaCorrente.class, conta.getCodConta());		
+				contaCorrente.setBanco(conta.getBanco());
+				contaCorrente.setPessoa(conta.getPessoa());
+				contaCorrente.setNumeroConta(conta.getNumeroConta());
+				contaCorrente.setNumeroAgencia(conta.getNumeroAgencia());
+				
+				transaction.commit();
+			} finally {
+				if (transaction.isActive()) {
+					transaction.rollback();
+				}
+			}
+		} finally {			
+			entityManager.close();
+			factory.close();			
+		}
+	}
+	
+	public void persist(Conta conta) {
+		try {
+			EntityTransaction transaction = entityManager.getTransaction();
+			try {
+				transaction.begin();
+				
+				ContaCorrente contaCorrente = new ContaCorrente();		
+				contaCorrente.setBanco(conta.getBanco());
+				contaCorrente.setPessoa(conta.getPessoa());
+				contaCorrente.setNumeroConta(conta.getNumeroConta());
+				contaCorrente.setNumeroAgencia(conta.getNumeroAgencia());
+
+				entityManager.persist(contaCorrente);
+				
+				transaction.commit();
+			} finally {
+				if (transaction.isActive()) {
+					transaction.rollback();
+				}
+			}	
+		} finally {
+			entityManager.close();
+			factory.close();			
+		}
+	}
+	
+	public void remove(Conta conta) {
+		try {
+			EntityTransaction transaction = entityManager.getTransaction();
+			try {
+				transaction.begin();
+				entityManager.remove(conta);
+			
+				transaction.commit();
+			} finally {
+				if (transaction.isActive()) {
+					transaction.rollback();
+				}
+			}
+		} finally {
+			entityManager.close();
+			factory.close();	
+		}
+	}
+	
+	public ContaCorrente buscarContaPeloId(int id) {
+		ContaCorrente contaCorrente = null;
+		try {
+			EntityTransaction transaction = entityManager.getTransaction();
+			try {
+				transaction.begin();
+				
+				contaCorrente = entityManager.find(ContaCorrente.class, id);	
+				contaCorrente.setCodConta(contaCorrente.getCodConta());
+				contaCorrente.setBanco(contaCorrente.getBanco());
+				contaCorrente.setPessoa(contaCorrente.getPessoa());
+				contaCorrente.setNumeroConta(contaCorrente.getNumeroConta());
+				contaCorrente.setNumeroAgencia(contaCorrente.getNumeroAgencia());
+
+				entityManager.remove(contaCorrente);
+			
+				transaction.commit();
+			} finally {
+				if (transaction.isActive()) {
+					transaction.rollback();
+				}
+			}
+		} finally {
+			entityManager.close();
+			factory.close();	
+		}		
+		return contaCorrente;
+	}
 }
--- sigecon/src/br/sigecon/util/Main.java
+++ sigecon/src/br/sigecon/util/Main.java
@@ -1,60 +1,64 @@
 package br.sigecon.util;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-
-import br.sigecon.beans.ContaCorrente;
-import br.sigecon.beans.Lancamento;
-import br.sigecon.beans.TipoLancamento;
-import br.sigecon.beans.Transferencia;
+import br.sigecon.beans.Banco;
+import br.sigecon.beans.Conta;
+import br.sigecon.beans.Pessoa;
+import br.sigecon.daos.ContasDAO;
 
 public class Main {
 	public static void main(String[] args) {
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("persist");
-		EntityManager entityManager = factory.createEntityManager();
-		EntityTransaction transaction = entityManager.getTransaction();
+//		EntityManagerFactory factory = Persistence.createEntityManagerFactory("persist");
+//		EntityManager entityManager = factory.createEntityManager();
+//		EntityTransaction transaction = entityManager.getTransaction();
 		
-		transaction.begin();
+//		transaction.begin();
 
-//		ContaCorrente contaCorrente = new ContaCorrente();		
-//		contaCorrente.setBanco(entityManager.find(Banco.class, 1));
-//		contaCorrente.setPessoa(entityManager.find(Pessoa.class, 1));
-//		contaCorrente.setNumeroConta(1235);
-//		contaCorrente.setNumeroAgencia(4454);
+		Banco banco = new Banco();
+		banco.setCodBanco(1);
+		
+		Pessoa pesssoa = new Pessoa();
+		pesssoa.setCodPessoa(1);		
+		
+//		ContaCorrente contaCorrente = new ContaCorrente();
+//		contaCorrente.setCodConta(9);
+//		contaCorrente.setBanco(banco);
+//		contaCorrente.setPessoa(pesssoa);
+//		contaCorrente.setNumeroConta(567);
+//		contaCorrente.setNumeroAgencia(4451);
+	
+		ContasDAO dao = new ContasDAO();
+		
+		Conta conta = dao.buscarContaPeloId(9);
+		dao.remove(conta);
+		
+//		dao.persist(contaCorrente);
+		
+//		Transferencia transferencia = new Transferencia();
+//		transferencia.setContaCorrente(entityManager.find(ContaCorrente.class, 8));
+//		transferencia.setContaDestino(entityManager.find(ContaCorrente.class, 8));
+//		transferencia.setDataLancamento(new Date());
+//		transferencia.setTipoLancamento(entityManager.find(TipoLancamento.class, 1));
+//		transferencia.setMotivoLancamento("teste");
+//		transferencia.setValorLancamento(100L);
+//		
+//		entityManager.persist(transferencia);
+		
+//		ContaCorrente contaCorrente = entityManager.find(ContaCorrente.class, 8);
+//		Lancamento lancamento = entityManager.find(Lancamento.class, 4);
+//
+//		if (contaCorrente.getLancamentoList() == null) {
+//			List<Lancamento> lancamentos = new ArrayList<Lancamento>();
+//			lancamentos.add(lancamento);
+//			contaCorrente.setLancamentoList(lancamentos);			
+//		} else {
+//			contaCorrente.getLancamentoList().add(lancamento);
+//		}
+//		
 //		entityManager.persist(contaCorrente);
-
-		Transferencia transferencia = new Transferencia();
-		transferencia.setContaCorrente(entityManager.find(ContaCorrente.class, 8));
-		transferencia.setContaDestino(entityManager.find(ContaCorrente.class, 8));
-		transferencia.setDataLancamento(new Date());
-		transferencia.setTipoLancamento(entityManager.find(TipoLancamento.class, 1));
-		transferencia.setMotivoLancamento("teste");
-		transferencia.setValorLancamento(100L);
-		
-		entityManager.persist(transferencia);
-		
-		ContaCorrente contaCorrente = entityManager.find(ContaCorrente.class, 8);
-		Lancamento lancamento = entityManager.find(Lancamento.class, 4);
-
-		if (contaCorrente.getLancamentoList() == null) {
-			List<Lancamento> lancamentos = new ArrayList<Lancamento>();
-			lancamentos.add(lancamento);
-			contaCorrente.setLancamentoList(lancamentos);			
-		} else {
-			contaCorrente.getLancamentoList().add(lancamento);
-		}
-		
-		entityManager.persist(contaCorrente);
-		
-		transaction.commit();
-		
-		entityManager.close();
-		factory.close();
+//		
+//		transaction.commit();
+//		
+//		entityManager.close();
+//		factory.close();
 	}
 }
